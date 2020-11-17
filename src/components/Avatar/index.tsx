7bea111bc194a3cb8fd2331595bfbcdf6bb175da
mojahige <mojahige@gmail.com>
2020-11-17 21:07:10 +0900

    add modifier class name

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,17 +1,21 @@
 import './index.scss';
 import { defineComponent } from 'vue';
 
-export default defineComponent({
+export const Avatar = defineComponent({
   name: 'Avatar',
   props: {
     name: {
       required: true,
       type: String
+    },
+    size: {
+      type: String,
+      default: ''
     }
   },
   render() {
     return (
-      <div class="Avatar">
+      <div class={this.size ? `Avatar -${this.size}` : 'Avatar'}>
         <div class="Avatar__image" />
         <div class="Avatar__text">{this.name}</div>
       </div>
